d9e400ba32bf7b2cd3fc2643d56ac24f6130550e
SamuelPalacioMesa <60185264+SamuelPalacioMesa@users.noreply.github.com>
2020-01-29 22:33:37 -0500

    DESAFIOTARIFAS

---
+++ Desafiotarifa01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace DESAFIOTARIFA
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            Console.Write("Ingrese su salario en pesos : ");
+           double salario = double.Parse(Console.ReadLine());
+            if (salario < (2 * 877803))
+            {
+                Console.WriteLine("Su tarifa es tipo A");
+            }
+            else if ((2 * 877803) <= salario && salario <= (4 * 877803))
+            {
+                Console.WriteLine("Su tarifa es tipo B");
+            }
+           
+            else
+            {
+                Console.WriteLine("Su tarifa es tipo C");
+            }
+
+        }
+    }
+}
---
+++ Desafiotarifa02.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace DESAFIOTARIFA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese su salario en pesos : ");
+            double salario = double.Parse(Console.ReadLine());
+            if (salario < (2 * 877803))
+            {
+                Console.WriteLine("Su tarifa es tipo A");
+                Console.WriteLine("El valor de su cuota moderadora es de 3400");
+            }
+            else if ((2 * 877803) <= salario && salario <= (5 * 877803))
+            {
+                Console.WriteLine("Su tarifa es tipo B");
+                Console.WriteLine("El valor de su cuota moderadora es de 13500");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa es tipo C");
+                Console.WriteLine("El valor de su cuota moderadora es de 35600");
+            }
+        }
+    }
+}
